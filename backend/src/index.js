1dcb229a9c15c8d6b89f1731c45c577f1ed0558c
madelineekim <149644675+madelineekim@users.noreply.github.com>
2025-02-22 16:22:26 -0500

    production stuff

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 import authRoutes from "../src/routes/auth.route.js"
 import dataRoutes from "../src/routes/data.route.js"
 
@@ -9,6 +10,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,6 +22,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes)
 app.use("/api/data", dataRoutes)
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
